6c61e03d88260edd66c4e357fc83a461c957ce32
Harsha Teja Kanna <h7kanna@gmail.com>
2023-07-19 22:29:37 -0500

    Added type safe workflow definition trait and function abstractions

--- sdk/src/lib.rs
+++ sdk/src/lib.rs
@@ -48,6 +48,8 @@ mod activity_context;
 mod app_data;
 pub mod interceptors;
 mod payload_converter;
+pub mod prelude;
+pub mod workflow;
 mod workflow_context;
 mod workflow_future;
 
@@ -65,7 +67,6 @@ use crate::{
 use anyhow::{anyhow, bail, Context};
 use app_data::AppData;
 use futures::{future::BoxFuture, FutureExt, StreamExt, TryFutureExt, TryStreamExt};
-use serde::Serialize;
 use std::{
     any::{Any, TypeId},
     cell::RefCell,
